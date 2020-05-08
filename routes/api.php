dd15bbb50823b4a92c54a0d2918587de504501a0
Ernest <emuroiwa@gmail.com>
2020-05-08 10:14:55 +0200

    User controller for JWT

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('register', 'API\UserController@register');
+Route::post('login', 'API\UserController@login');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
